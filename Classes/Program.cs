f2da798c492417d6f56d4b7a42d800f02ee28a5e
nwhang1 <nicholasjwhang@gmail.com>
2024-04-02 09:00:18 -0700

    Class created, make, model, year added, prints to console

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Classes
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+
+            myCar.Make = "Toyota";
+            myCar.Model = "RAV4";
+            myCar.year = 2020;
+
+            Console.WriteLine($"Make: {myCar.Make}, Model : {myCar.Model} , Year: {myCar.year}");
         }
     }
 }
